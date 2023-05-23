d438c96d46ee5141999495f67bcd44305536d769
Sofia Lyvarenko <livarenko.sofia@gmail.com>
2023-05-23 22:34:48 +0300

    редактирование ошибок

--- controllers/movies.js
+++ controllers/movies.js
@@ -31,9 +31,9 @@ module.exports.createMovie = (req, res, next) => {
 };
 
 module.exports.deleteMovie = (req, res, next) => {
-  const { movieId } = req.params;
+  const { _id } = req.params;
   const userId = req.user._id;
-  Movie.findById(movieId)
+  Movie.findById(_id)
     .populate('owner')
     .then((movie) => {
       if (!movie) {
